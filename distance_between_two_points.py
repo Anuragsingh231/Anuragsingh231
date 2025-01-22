d1851bbfb817875fe21f09a630b218eaf916bdce
Anuragsingh231 <120211606+Anuragsingh231@users.noreply.github.com>
2025-01-22 14:10:43 +0530

    Create distance_between_two_points.py

---
+++ distance_between_two_points.py
@@ -0,0 +1,8 @@
+x1=int(input("Enter point X1"))
+x2=int(input("Enter point X2"))
+y1=int(input("Enter point Y1"))
+y2=int(input("Enter point Y2"))
+
+a=(x2-x1)**2+(y2-y1)**2
+distance=a**0.5
+print("Distance between two points is:",distance)
